aa569aee78a49dda058324cc808b6580367d0b93
S Nandhakishor <nandakishornair17@gmail.com>
2022-04-19 13:59:49 +0530

    2nd js

--- js/form.js
+++ js/form.js
@@ -1,38 +1,68 @@
 
-var first_name = document.getElementById("first_name");
-var last_name = document.getElementById("last_name");
-
-var adress = document.getElementById("inputAddress");
-
-
-function validateemail()
-{
-    let email = document.getElementById("inputEmail4");
+// function validateemail()
+// {
+    // let email = document.getElementById("inputEmail4");
     // let emailmsg = document.getElementById("emailmsg");
-   let regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(regexp.test(email.value)){
-      // incorrect.classList.remove("invalid");
-      return true;
+  //  let regexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+   
+  //  if(regexp.test(email))
+  //  {
+      // return true;
   
-    }
-    else 
-     {  // correct.classList.remove("invalid");
-        return false;
-    }
+    // }
+    // else 
+    //  {
+        // return false;
+    //  }
 
-}
+// }
 function phonenumber()
 {
-  let phoneno = document.getElementById("input.Ph");
+  let phoneno = document.getElementById("input.Ph").value;
   let phoneno1 = /^\d{10}$/;
   let phoneno2 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{7})$/;
+  let msg1 = document.getElementById("checkph"); 
+  
   // let phoneno3 = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
-  if(( phoneno.value.match(phoneno1))||( phoneno.value.match(phoneno2)))
+  if(( phoneno.match(phoneno1))||( phoneno.match(phoneno2)))
         {
+          msg1.style.color="green";
+          msg1.textContent="✔ valid phonenumber"; 
       return true;
         }
       else
         {
-        return false;
+           msg1.style.color="brown";
+           msg1.textContent="X invalid phonenumber"; 
+
+          return false;
         }
 }
+function pswdcheck()
+{
+  let pswd1=document.getElementById("inputPassword4");
+  let pswd2=document.getElementById("inputPassword3");
+  let msg = document.getElementById("checkpassword");
+  if(pswd1.value==pswd2.value)
+  {
+    msg.style.color="green";
+    msg.textContent="✔ password  match";
+    return true;
+    
+  }
+  else{
+    msg.style.color="brown";
+    msg.textContent="X password do not match";
+    return false;
+  }
+}
+// function checkemail()
+// { let va = document.getElementsById("va");
+// let inv =  document.getElementById("inv");
+  // if (validateemail()==true) {
+    // va.style.display = "none";
+  // }
+  // else{
+    // inv.style.display="none";
+  // }
+// }
